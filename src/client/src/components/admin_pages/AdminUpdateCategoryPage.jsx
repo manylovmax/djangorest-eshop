0b217d705171b7fdb97683cc7765befdaaf04fe1
manylovmax <maximalist000@gmail.com>
2024-11-11 18:06:21 +0300

    обновление категорий

--- src/client/src/components/admin_pages/AdminUpdateCategoryPage.jsx
+++ src/client/src/components/admin_pages/AdminUpdateCategoryPage.jsx
@@ -1,40 +1,76 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import axios from "axios";
+
+import constants from "../../constants";
 
 
-export default function AdminCreateCategoryPage() {
-    const categories = [
-        {id: 1, title: "Электроника", path: "/1/"},
-        {id: 2, title: "Одежда", path: "/2/"},
-    ];
-    const [selectedCategory, setSelectedCategory] = useState(null);
-    const [categoryName, setCategoryName] = useState("");
-    function onChange(event) {
-        let selected = categories.filter(category => category.id == event.target.value);
-        setSelectedCategory(selected[0]);
-    }
+export default function AdminCreateCategoryPage () {
+    const navigate = useNavigate();
+
+    const [parentCategoryId, setParentCategoryId] = useState("");
+    const [parentCategory, setParentCategory] = useState(null);
+    const [categories, setCategories] = useState([]);
+    const [categoryTitle, setCategoryTitle] = useState("");
+    const [category, setCategory] = useState(null);
+    const [categoryAssignable, setCategoryAssignable] = useState(false);
+
+    const {categoryId} = useParams();
+
+    useEffect(() => {
+        axios.get(constants.SERVER_ADDRESS + "/products/all-categories/").then(response => {
+            setCategories(response.data);
+        });
+        axios.get(`${constants.SERVER_ADDRESS}/products/product-category/${categoryId}/`).then(response => {
+            setCategory(response.data);
+            setCategoryAssignable(response.data?.assignable);
+            setCategoryTitle(response.data?.title);
+            if (response.data.parent) {
+                setParentCategoryId(response.data?.parent);
+                axios.get(`${constants.SERVER_ADDRESS}/products/product-category/${response.data.parent}/`).then(response => setParentCategory(response.data));
+            }
+        })
+    }, [])
+
     function updateCategory() {
+        axios.put(`${constants.SERVER_ADDRESS}/products/product-category/${categoryId}/`, {
+            title: categoryTitle,
+            parent: parentCategoryId ? parentCategoryId : null,
+            assignable: categoryAssignable
+        }).then(response => navigate("/admin/categories"));
+    }
 
+    function setParentCategoryHandler(id) {
+        setParentCategoryId(id);
+        const category = categories.filter(category => category.id == id)[0];
+        setParentCategory(category);
     }
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-12">
-                    <h1>Редактировать категорию продуктов</h1>
-                    <div className="mb-3"><label htmlFor="parent_category">Родительская категория:</label>
-                        <select className="form-select" onChange={onChange} name="parent_category" value={selectedCategory? selectedCategory.id : 0} >
+                    <h1>Создать категорию</h1>
+                    <div className="mb-3">Родительская категория:
+                        <select className="form-select" onChange={(e) => setParentCategoryHandler(e.target.value)} name="parent_category" value={parentCategoryId} >
                             <option value="0">Нет</option>
                             {categories.map((category, idx) => (
                                 <option key={idx} value={category.id}>{category.title}</option>
                             ))}
                         </select>
-                        <span>ID: {selectedCategory ? selectedCategory.id: ""} </span><span>path: {selectedCategory ? selectedCategory.path: ""}</span></div>
+                        <span>ID: {parentCategoryId ? parentCategoryId : ""} </span><span>Путь: {parentCategory ? parentCategory.path : ""}</span></div>
                     <div className="mb-3">
                         <label htmlFor="category-name">Название категории:</label>
-                        <input className="form-control" onInput={(e) => setCategoryName(e.target.value)} id="category-name" name="category-name" type="text" value={categoryName}/>
+                        <input className="form-control" onInput={(e) => setCategoryTitle(e.target.value)} id="category-name" name="category-name" type="text" value={categoryTitle}/>
                     </div>
-                    <button className="btn btn-primary" onClick={updateCategory}>Сохранить</button>
-                    <a className="btn btn-link" href="/admin/categories/" >Отмена</a>
+                    <div className="mb-3">
+                      <div className="form-check">
+                        <input name="assignable" className="form-check-input"
+                         type="checkbox" id="assignable" onChange={(e) => setCategoryAssignable(e.target.checked)} checked={categoryAssignable}/>
+                        <label className="form-check-label" htmlFor="assignable">Назначаемая</label>
+                      </div>
+                    </div>
+                    <button className="btn btn-primary" onClick={updateCategory}>Обновить</button>
                 </div>
             </div>
         </div>
