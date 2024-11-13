54a60c84e26e3c6d5b34c3cba053f0a7661ea224
manylovmax <maximalist000@gmail.com>
2024-11-13 20:06:00 +0300

    AdminAttributeNamesPage: фикс кнопки создат и удаления имен

--- src/client/src/components/admin_pages/AdminAttributeNamesPage.jsx
+++ src/client/src/components/admin_pages/AdminAttributeNamesPage.jsx
@@ -52,7 +52,7 @@ export default class AdminAttributeNamesPage extends React.Component {
     handleDelete(id, title) {
         const answer = confirm("Удалить \"" + title + "\"?");
         if (answer) {
-            axios.delete(`${constants.SERVER_ADDRESS}/products/attribute-category/${id}/`)
+            axios.delete(`${constants.SERVER_ADDRESS}/products/attribute-name/${id}/`)
             .then(() => this.getPage(1));
         }
     }
@@ -61,7 +61,7 @@ export default class AdminAttributeNamesPage extends React.Component {
         return (
             <>
                 <h1>Названия атрибутов</h1>
-                <Link to="/admin/attribute-categories/create" className="btn btn-success">Создать</Link>
+                <Link to="/admin/attribute-names/create" className="btn btn-success">Создать</Link>
                 <table className="table">
                     <thead>
                         <tr>
