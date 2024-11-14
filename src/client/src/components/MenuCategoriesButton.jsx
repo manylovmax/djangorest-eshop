d7d8c737d9c6f47b82a2df2e3af3337100e55b7c
manylovmax <maximalist000@gmail.com>
2024-11-14 18:29:23 +0300

    MenuCategoriesButton: рендеринг категория и субкатегорий до 3 уровня

--- src/client/src/components/MenuCategoriesButton.jsx
+++ src/client/src/components/MenuCategoriesButton.jsx
@@ -1,32 +1,45 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+import constants from "../constants";
+
 
 export default function MenuCategoriesButton() {
     const [menuVisible, setMenuVisible] = useState(false);
-    function handleClick() {
-        setMenuVisible(!menuVisible);
+    const [categories, setCategories] = useState([]);
+    const [subcategories, setSubCategories] = useState([]);
+
+    useEffect(() => {
+        axios.get(constants.SERVER_ADDRESS + "/products/get-categories-tree/").then(response => {
+            setCategories(response.data);
+            setSubCategories(response.data[0].subcategories);
+        }
+        )
+    }, []);
+
+    function handleShowSubcategories(categoryId) {
+        const newSubCategories = categories.filter(c => c.id == categoryId)[0]?.subcategories;
+        setSubCategories(newSubCategories);
     }
 
     return (
         <>
-            <div onClick={handleClick} className="menu-categories-button btn btn-primary">Каталог</div>
+            <div onClick={() => setMenuVisible(!menuVisible)} className="menu-categories-button btn btn-primary">Каталог</div>
             <div className={"menu-categories-content " + (menuVisible ? "" : "d-none")}>
-                <div>Электроника</div>
-                <div>Одежда</div>
-                <div>Обувь</div>
-                <div>Дом и сад</div>
-                <div>Детские товары</div>
-                <div>Красота и здоровье</div>
-                <div>Бытовая техника</div>
-                <div>Спорт и отдых</div>
-                <div>Строительство и ремонт</div>
-                <div>Продукты питания</div>
-                <div>Аптека</div>
-                <div>Товары для животных</div>
-                <div>Книги</div>
-                <div>Туризм, рыбалка, охота</div>
-                <div>Автотовары</div>
-                <div>Мебель</div>
-                <div>Хобби и творчество</div>
+                <div className="menu-categories-content_categories first-level-category">
+                { categories.map((category, idx) => (
+                    <div key={idx} onMouseOver={() => handleShowSubcategories(category.id)} onClick={() => showSubcategories(categoy.id)}>{category.title}</div>
+                ))}
+                </div>
+                <div className="menu-categories-content_subcategories">
+                    { subcategories.map((subcategory, idx) => (
+                        <div key={idx} className="second-level-category">{subcategory.title}
+                        { subcategory.subcategories.map((subsubcategory, idx) => (
+                            <div key={idx} className="third-level-category">{subsubcategory.title}</div>
+                        ))}
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     )
