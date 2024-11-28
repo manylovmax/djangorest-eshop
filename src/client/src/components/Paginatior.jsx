a80f9b3404daab9e4644d638916507a4f3b17126
manylovmax <maximalist000@gmail.com>
2024-11-28 14:46:56 +0300

    фикс пагинатора при отсутсвии страниц

--- src/client/src/components/Paginatior.jsx
+++ src/client/src/components/Paginatior.jsx
@@ -6,27 +6,30 @@ export default function Paginator({pageNumber, totalPages, hasNextPage, hasPrevi
             callback();
         }
     }
-    return (
-        <nav>
-            <ul className="pagination justify-content-center">
-                <li className={"page-item " + (hasPreviousPage ? "" : "disabled")}>
-                    <a className="page-link" onClick={() => callIfTrue(() => onChangePage(pageNumber - 1), hasPreviousPage)}>Предыдущая</a>
-                </li>
-                { pageNumber != 1 
-                    ? <li className="page-item"><a className="page-link" onClick={() => callIfTrue(() => onChangePage(1), true)}>1</a></li>
-                    : "" 
-                }
-                
-                <li className="page-item active"><a className="page-link">{pageNumber}</a></li>
+    if (totalPages)
+        return (
+            <nav>
+                <ul className="pagination justify-content-center">
+                    <li className={"page-item " + (hasPreviousPage ? "" : "disabled")}>
+                        <a className="page-link" onClick={() => callIfTrue(() => onChangePage(pageNumber - 1), hasPreviousPage)}>Предыдущая</a>
+                    </li>
+                    { pageNumber != 1 
+                        ? <li className="page-item"><a className="page-link" onClick={() => callIfTrue(() => onChangePage(1), true)}>1</a></li>
+                        : "" 
+                    }
+                    
+                    <li className="page-item active"><a className="page-link">{pageNumber}</a></li>
 
-                { pageNumber != totalPages
-                    ? <li className="page-item"><a className="page-link" onClick={() => callIfTrue(() => onChangePage(totalPages), true)}>{totalPages}</a></li>
-                    : "" 
-                }
-                <li className={"page-item " + (hasNextPage ? "" : "disabled")}>
-                    <a className="page-link" onClick={() => callIfTrue(() => onChangePage(pageNumber + 1), hasNextPage)}>Следующая</a>
-                </li>
-            </ul>
-        </nav>
-    )
+                    { pageNumber != totalPages
+                        ? <li className="page-item"><a className="page-link" onClick={() => callIfTrue(() => onChangePage(totalPages), true)}>{totalPages}</a></li>
+                        : "" 
+                    }
+                    <li className={"page-item " + (hasNextPage ? "" : "disabled")}>
+                        <a className="page-link" onClick={() => callIfTrue(() => onChangePage(pageNumber + 1), hasNextPage)}>Следующая</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    else
+        return "";
 }
